0964731de0579a35c2f531b9dbcf851c15891520
nordluma <maxnordlund.95@hotmail.com>
2023-11-29 14:27:44 +0200

    feat: add paragraph to page

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,8 @@
     <Counter />
   </div>
 
+    <p>yoyoyoyoy</p>
+
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
